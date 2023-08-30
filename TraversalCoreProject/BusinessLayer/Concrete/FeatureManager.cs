d07e44aec6db50ef5d9c1db7b88e1960f4f602b5
keremkul0 <keremkul0@outlook.com>
2023-08-30 17:20:15 +0300

    News

---
+++ TraversalCoreProject/BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager:IFeature1Service
+    {
+        IFeature1Dal _feature1Dal;
+
+        public FeatureManager(IFeature1Dal feature1Dal) 
+        {
+            _feature1Dal=feature1Dal;
+        }
+
+        public void TAdd(Feature1 t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Feature1 t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Feature1 TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Feature1> TGetList()
+        {
+            return _feature1Dal.GetList();
+        }
+
+        public void TUpdate(Feature1 t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatures1Dal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    }
+}
