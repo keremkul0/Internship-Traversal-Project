82b5fe56691a8e97fcb4b55d9da6c80da6a21062
keremkul0 <keremkul0@outlook.com>
2023-08-31 17:33:00 +0300

    Devam

---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_SubAbout.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SubAbout :ViewComponent
+    {
+        SubAboutManager _subAboutmanager= new SubAboutManager(new EfSubAboutDal());
+        public IViewComponentResult Invoke() 
+        {
+            var values = _subAboutmanager.TGetList();
+            return View(values); 
+        }
+    }
+}
---
+++ TraversalCoreProject/TraversalCoreProject/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        TestimonialManager testimonialManager=new TestimonialManager(new EfTestimonialsDal());
+        public IViewComponentResult Invoke()
+        {
+            var value= testimonialManager.TGetList();
+            return View(value);
+        }
+    }
+}
