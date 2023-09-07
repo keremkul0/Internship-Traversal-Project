e53de24762fb77310591f3d6d1ed5acf8342f8d4
keremkul0 <keremkul0@outlook.com>
2023-09-07 12:08:43 +0300

    Update Program.cs

--- TraversalCoreProject/TraversalCoreProject/Program.cs
+++ TraversalCoreProject/TraversalCoreProject/Program.cs
@@ -1,8 +1,21 @@
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Context>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddMvc(config =>
+{
+	var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+	config.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddMvc();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
