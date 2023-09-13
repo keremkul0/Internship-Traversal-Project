4ecbcf902bac0fed3246edbacdc751dfd9e46d0d
keremkul0 <keremkul0@outlook.com>
2023-09-13 16:23:55 +0300

    Dosya eklendi

---
+++ TraversalCoreProject/TraversalCoreProject/Models/UserSignInViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TraversalCoreProject.Models
+{
+    public class UserSignInViewModel
+    {
+        [Required(ErrorMessage ="Pleas Enter Your Username")]
+        public string Username { get; set; }=string.Empty;
+
+        [Required(ErrorMessage = "Pleas Enter Your Password")]
+        public string Password { get; set; }= string.Empty;
+    }
+}
