87742c5f3b14463f3479145d5bee00b17d75bd0e
keremkul0 <keremkul0@outlook.com>
2023-09-13 12:05:40 +0300

    Revert "rename"
    This reverts commit 22cde1dfd4b2ec249c6c0bd4c935a1cc36c357a9.

--- TraversalCoreProject/TraversalCoreProject/Controllers/LoginController.cs
+++ TraversalCoreProject/TraversalCoreProject/Controllers/LoginController.cs
@@ -8,49 +8,49 @@ namespace TraversalCoreProject.Controllers
 {
 	[AllowAnonymous]
 	public class LoginController : Controller
-	{
-		private readonly UserManager<AppUser> _userManager;
-		public LoginController(UserManager<AppUser> userManager)
-		{
-			_userManager = userManager;
-		}
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
-		[HttpGet]
-		public IActionResult SignUp()
-		{
-			return View();
-		}
-		[HttpPost]
-		public async Task<IActionResult> SignUp(UserRegisterViewModel p)
-		{
-			AppUser appUser = new()
-			{
-				Name = p.Name,
-				Surname = p.Surname,
-				Email = p.Mail,
-				UserName = p.UserName,
-			};
-			if (p.Password == p.ConfirmPassword)
-			{
-				var result = await _userManager.CreateAsync(appUser, p.Password);
-				if (result.Succeeded)
-				{
-					return RedirectToAction("SignIn");
-				}
-				else
-				{
-					foreach (var item in result.Errors)
-					{
-						ModelState.AddModelError("", item.Description);
-					}
-				}
-			}
-			return View(p);
-		}
-		[HttpGet]
-		public IActionResult SingIn()
-		{
-			return View();
-		}
-	}
+        [HttpGet]
+        public IActionResult SingUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SingUp(UserRegisterViewModel p)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = p.Name,
+                Surname = p.Surname,
+                Email = p.Mail,
+                UserName = p.UserName,
+            };
+            if (p.Password == p.ConfirmPassword)
+            {
+                var result=await _userManager.CreateAsync(appUser,p.Password);
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else
+                {
+                    foreach(var item  in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(p);
+        }
+        [HttpGet]
+        public IActionResult SingIn()
+        {
+            return View();
+        }
+    }
 }
